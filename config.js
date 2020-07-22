847180546f767a10b2942d3e27a94d8f70776b39
Rascafr <rascafr@gmail.com>
2020-07-22 13:04:09 +0200

    + base config major/minor/patch & repos

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+    // ar drone 1.0 or 2.0?
+    ardroneVersion: 2,
+
+    // from 1.x.x to 3.x.x
+    majorFrom: 1,
+    majorTo: 3,
+
+    // from x.0.x to x.10.x
+    minorFrom: 0,
+    minorTo: 10,
+
+    // from x.x.0 to x.x.20
+    patchFrom: 0,
+    patchTo: 20,
+
+    // repository url function generator to use
+    repository: (x, y, z) => `http://www.digitalsirup.com/downloads/ardronefirmware/${x}.${y}.${z}/ardrone${module.exports.ardroneVersion}_update.plf`
+}
